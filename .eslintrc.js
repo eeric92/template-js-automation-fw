4e8c0fc592e07a36c48f054c66ea838925b899e3
Eric <sanceric92@gmail.com>
2023-01-01 18:24:52 +0100

    ESLint and Prettier working together

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-	env: {
-		browser: true,
-		commonjs: true,
-		es2021: true,
-		es6: true
-	},
-	extends: ["eslint:recommended", "prettier"],
-	overrides: [],
-	parserOptions: {
-		ecmaVersion: 12
-	},
-	rules: {
-		indent: ["error", "tab"],
-		"linebreak-style": ["error", "unix"],
-		quotes: ["error", "double"],
-		semi: ["error", "always"]
-	}
+    env: {
+        browser: true,
+        commonjs: true,
+        es2021: true,
+        es6: true,
+    },
+    extends: ["eslint:recommended", "prettier"],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {
+        indent: "off",
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+    },
 };
--- test/e2e/mocha-tests/SecondTest.js
+++ test/e2e/mocha-tests/SecondTest.js
@@ -5,72 +5,64 @@ const firefox = require("selenium-webdriver/firefox");
 const chrome = require("selenium-webdriver/chrome");
 
 const screen = {
-	width: 640,
-	height: 480,
+    width: 640,
+    height: 480,
 };
 
 let driver;
 describe("Browser test - HEADLESS MODE", function () {
-	it("Firefox browser test - HEADLESS", async function () {
-		driver = await new Builder()
-			.forBrowser("firefox")
-			.setFirefoxOptions(
-				new firefox.Options()
-					.headless()
-					.windowSize(screen)
-			)
-			.build();
-		await driver.get("https://google.com");
-		await driver.findElement(By.id("L2AGLb")).click();
-		await driver.sleep(1000);
-		await driver
-			.findElement(
-				By.xpath(
-					"/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"
-				)
-			)
-			.sendKeys("patata", Key.RETURN);
-		await driver.sleep(1000);
-		const text = await driver
-			.findElement(By.id("pTwnEc"))
-			.getText();
-		assert.strictEqual(
-			text,
-			"Todo\nImágenes\nVídeos\nNoticias\nShopping\nMás\nHerramientas"
-		);
-		await driver.quit();
-	});
+    it("Firefox browser test - HEADLESS", async function () {
+        driver = await new Builder()
+            .forBrowser("firefox")
+            .setFirefoxOptions(
+                new firefox.Options().headless().windowSize(screen)
+            )
+            .build();
+        await driver.get("https://google.com");
+        await driver.findElement(By.id("L2AGLb")).click();
+        await driver.sleep(1000);
+        await driver
+            .findElement(
+                By.xpath(
+                    "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"
+                )
+            )
+            .sendKeys("patata", Key.RETURN);
+        await driver.sleep(1000);
+        const text = await driver.findElement(By.id("pTwnEc")).getText();
+        assert.strictEqual(
+            text,
+            "Todo\nImágenes\nVídeos\nNoticias\nShopping\nMás\nHerramientas"
+        );
+        await driver.quit();
+    });
 
-	it("Chrome browser test - HEADLESS", async function () {
-		driver = await new Builder()
-			.forBrowser("chrome")
-			.setChromeOptions(
-				new chrome.Options()
-					.headless()
-					.windowSize(screen)
-			)
-			.build();
-		await driver.get("https://google.com");
-		await driver.findElement(By.id("L2AGLb")).click();
-		await driver.sleep(1000);
-		await driver
-			.findElement(
-				By.xpath(
-					"/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"
-				)
-			)
-			.sendKeys("patata", Key.RETURN);
-		await driver.sleep(1000);
-		const text = await driver
-			.findElement(By.id("pTwnEc"))
-			.getText();
-		assert.strictEqual(
-			text,
-			"Todo\nImágenes\nVídeos\nNoticias\nShopping\nMás\nHerramientas"
-		);
-	});
+    it("Chrome browser test - HEADLESS", async function () {
+        driver = await new Builder()
+            .forBrowser("chrome")
+            .setChromeOptions(
+                new chrome.Options().headless().windowSize(screen)
+            )
+            .build();
+        await driver.get("https://google.com");
+        await driver.findElement(By.id("L2AGLb")).click();
+        await driver.sleep(1000);
+        await driver
+            .findElement(
+                By.xpath(
+                    "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"
+                )
+            )
+            .sendKeys("patata", Key.RETURN);
+        await driver.sleep(1000);
+        const text = await driver.findElement(By.id("pTwnEc")).getText();
+        assert.strictEqual(
+            text,
+            "Todo\nImágenes\nVídeos\nNoticias\nShopping\nMás\nHerramientas"
+        );
+    });
 
-	after(async function () {
-		await driver.quit();
-	});
+    after(async function () {
+        await driver.quit();
+    });
 });
